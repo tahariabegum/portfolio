3cbf2abb8d5af8b26e750d02cdcbfe8ff436aba5
tahariabegum <taharia97@gmail.com>
2024-02-08 19:12:08 -0500

    about my page

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -1,7 +1,31 @@
+import './AboutMe.css'
+
+
 export default function AboutMe() {
     return (
-        <div>
-            
+        <div className = 'about-me'>
+            <div className = 'myname'>
+                <div className = 'first'> 
+                    T A H A R I A 
+                </div>
+                <br/> 
+                <div>
+                    B E G U M
+                </div>
+            </div>
+            <div className = 'links'>
+                <div>
+                    <a href = '/education'> Education </a> |
+                </div>
+                <div>
+                    <a href = '/workhistory'> Work History </a> | 
+                </div>
+                <div>
+                    <a href = '/skills'> Skills </a>
+                </div>
+            </div>
         </div>
+
+       
     )
 }
